009e1b45f520a698c38a90ebb91e7cc78e1de19f
AlexejHein <134226688+AlexejHein@users.noreply.github.com>
2024-02-02 19:32:57 +0100

    add sidenav

--- SaveDate/src/app/app-routing.module.ts
+++ SaveDate/src/app/app-routing.module.ts
@@ -2,14 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./Auth/login/login.component";
 import {DashboardComponent} from "./Main/dashboard/dashboard.component";
+import {AddDateComponent} from "./Management/add-date/add-date.component";
+import {EditDateComponent} from "./Management/edit-date/edit-date.component";
+import {ShowDateComponent} from "./Management/show-date/show-date.component";
 
 const routes: Routes = [
   {
-    path: '',
-    component: LoginComponent
+    path: '', component: LoginComponent
   }, {
-    path: 'dashboard',
-    component: DashboardComponent
+    path: 'dashboard', component: DashboardComponent
+  },
+  {
+    path: 'add-date', component: AddDateComponent
+  },
+  {
+    path: 'edit-date', component: EditDateComponent
+  },
+  {
+    path: 'show-date', component: ShowDateComponent
   }
 ];
 
